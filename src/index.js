c230a15dd752bd7a42ed358a1d8e8ea973f94990
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-08 04:27:57 -0400

    added stock image, imported into html content with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css"
+
+import stockRestImage from "./images/stockRestPhoto.jpg";
+
+const image = document.createElement("img");
+image.src = stockRestImage;
+
+document.getElementById("content").appendChild(image);

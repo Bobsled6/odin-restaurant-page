f77382b485ec217edd10c5ceacb6c38b7901fa4a
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-09 04:58:15 -0400

    changed photo, added headline and brief description of restauraunt

--- src/initLoad.js
+++ src/initLoad.js
@@ -1,11 +1,22 @@
-import stockRestImage from "./images/stockRestPhoto.jpg";
+import foodStockPhoto from "./images/foodStockPhoto.jpg";
 
 const image = document.createElement("img");
-
-image.src = stockRestImage;
+const imageFigure = document.createElement("figure");
+const imageRef = document.createElement("figcaption");
+const headline = document.createElement("h1");
+const restDescription = document.createElement("p");
+const content = document.getElementById("content");
+image.src = foodStockPhoto;
 
 
 export function initLoad() {
-    document.getElementById("content").appendChild(image);
+    imageRef.innerHTML = "Photo by No Revisions unsplash.com/@norevisions"
+    headline.innerHTML = "Come Down and Watch the Game at Ballgame Bar and Grill"
+    restDescription.innerHTML = "Ballgame Bar and Grill is your spot to enjoy the game and eat authentic ballpark concessions straight off the grill! Come enjoy beer, pizza, wings and more without the lines and outrageous ballpark prices. "
+    imageFigure.appendChild(image);
+    imageFigure.appendChild(imageRef);
+    content.appendChild(imageFigure);
+    content.appendChild(headline);
+    content.appendChild(restDescription);
 }
 

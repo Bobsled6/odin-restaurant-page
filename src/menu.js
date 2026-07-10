6f8fa869b7ed79fbf6169efb342dfc897b647cbd
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-10 05:45:12 -0400

    added menuCategory class to menu module

--- src/menu.js
+++ src/menu.js
@@ -13,15 +13,16 @@ const toppings = {
         
 class menuItem {
     
-    constructor (item,price,description) {
+    constructor (item,price,category) {
         const menuItemDiv = document.createElement("div");
         const itemDiv = document.createElement("div");
         const priceDiv = document.createElement("div");
-        const descriptionP = document.createElement("p");
+        const menuCat = document.getElementById(category);
+
+
 
         this.item = item;
         this.price = price;
-        this.description = description;
 
         menuItemDiv.id = (item);
         menuItemDiv.classList.add("menuItem");
@@ -32,42 +33,63 @@ class menuItem {
         priceDiv.classList.add("price");
         priceDiv.innerHTML = '$' + price;
         
-        descriptionP.classList.add("description");
-        descriptionP.innerHTML = description;
-
-        
         menuItemDiv.appendChild(itemDiv);
         menuItemDiv.appendChild(priceDiv);
-        menuItemDiv.appendChild(descriptionP);
-        content.appendChild(menuItemDiv);
-        console.log(menuItemDiv);
-        
 
+        menuCat.appendChild(menuItemDiv);
+    
     };
 
     
 }
 
+class menuCategory {
+
+    constructor(name,toppingsCat, description) {
+
+        const descriptionP = document.createElement("p");
+        descriptionP.classList.add("description");
+        descriptionP.innerHTML = description;
+
+        const menuCat = document.createElement("div");
+        menuCat.id = (name);
+        
+        const menuCatHead = document.createElement("h2");
+        menuCatHead.innerHTML = name;
+        menuCat.appendChild(menuCatHead);
+
+        const toppingList = document.createElement("ul");
+        toppings[toppingsCat].forEach((item) => {
+            let li = document.createElement("li");
+            li.innerHTML = item;
+            toppingList.appendChild(li);
+        })
+
+        console.log(menuCat);
+    
+       menuCat.appendChild(toppingList);
+       menuCat.appendChild(descriptionP);
+       content.appendChild(menuCat);
+
+    }
+}
+
 export function menu() {
     
     const condDiv = document.createElement("div");
-    const condHead = document.createElement("h1")
+    const condHead = document.createElement("h2")
     const condList = document.createElement("ul");
-    
-    condHead.innerHTML = "Condiments";
+    const menuHeader = document.createElement("h1");
 
+    menuHeader.innerHTML = "Menu";
+    content.appendChild(menuHeader);
+    
+    new menuCategory("Pizza", "pizzaToppings","Available in slices (Cheese or Pepperoni), Small(12 in.), or Large(18 in.). Extra toppings $1.00 each");
+    new menuCategory("Condiments", "condiments", "")
+    
+    new menuItem("Slice","2.00", "Pizza")
+    new menuItem("Small", "10", "Pizza")
+    new menuItem("Large","15", "Pizza")
 
-    toppings.condiments.forEach((item) => {
-        let li = document.createElement("li");
-        li.innerHTML = item;
-        condList.appendChild(li);
-    })
     
-    condDiv.appendChild(condHead);
-    condDiv.appendChild(condList);
-    content.appendChild(condDiv);
-    
-    
-    new menuItem("pizza","2.00","")
-    new menuItem("wings","10.00")
 }

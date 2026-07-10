89beb48caa762b0b4f05b09a05b8dfc8e63df823
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-10 01:51:30 -0400

    added toppings object to menu module

--- src/menu.js
+++ src/menu.js
@@ -2,11 +2,22 @@ const content = document.getElementById("content");
 
 class menuItem {
     
-    constructor (item,price, description) {
+    constructor (item,price,options,description) {
         const menuItemDiv = document.createElement("div");
         const itemDiv = document.createElement("div");
         const priceDiv = document.createElement("div");
         const descriptionP = document.createElement("p");
+
+
+        const toppings = {
+            pizzaToppings: ["Pepperoni", "Ham", "Sausage", "Chicken", "Broccoli", "Green Pepper", "Red Pepper", "Banana Pepper", "Jalapeno Pepper", "Red Pepper","Green Olives", "Black Olives", "Onion", "Mushroom"],
+            pizzaSauce: ["Traditional", "Ranch", "Buffalo", "BBQ"],
+            wingFlavors: ["Hot Buffalo", "Mild Buffalo", "BBQ", "Honey BBQ", "Lemon Pepper", "Teriyaki"],
+            wingSauce: ["Ranch", "Blue Cheese"],
+            burgerToppings: ["Bacon", "Lettuce", "Tomato", "Onion", "Pickle"],
+            cheeseTypes: ["Provolone", "American", "Cheddar", "Pepper Jack"],
+            condiments: ["Ketchup", "Mustard", "Mayonnaise", "Relish", "Hot Sauce", "BBQ Sauce", "Melted Cheddar"],
+        };
         
         this.item = item;
         this.price = price;
@@ -38,6 +49,6 @@ class menuItem {
 }
 
 export function menu() {
-    new menuItem("pizza","2.00")
+    new menuItem("pizza","2.00","")
     new menuItem("wings","10.00")
 }

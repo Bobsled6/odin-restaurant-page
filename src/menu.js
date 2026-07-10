a7d458f0e88f140a9ddae3b777dce7ab2945c173
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-10 03:31:39 -0400

    moved toppings object to global scope of menu module, added condiments div and list to menu function

--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,24 @@
 const content = document.getElementById("content");
 
+
+const toppings = {
+    pizzaToppings: ["Pepperoni", "Ham", "Sausage", "Chicken", "Broccoli", "Green Pepper", "Red Pepper", "Banana Pepper", "Jalapeno Pepper", "Red Pepper","Green Olives", "Black Olives", "Onion", "Mushroom"],
+    pizzaSauce: ["Traditional", "Ranch", "Buffalo", "BBQ"],
+    wingFlavors: ["Hot Buffalo", "Mild Buffalo", "BBQ", "Honey BBQ", "Lemon Pepper", "Teriyaki"],
+    wingSauce: ["Ranch", "Blue Cheese"],
+    burgerToppings: ["Bacon", "Lettuce", "Tomato", "Onion", "Pickle"],
+    cheeseTypes: ["Provolone", "American", "Cheddar", "Pepper Jack"],
+    condiments: ["Ketchup", "Mustard", "Mayonnaise", "Relish", "Hot Sauce", "BBQ Sauce", "Melted Cheddar"],
+};
+        
 class menuItem {
     
-    constructor (item,price,options,description) {
+    constructor (item,price,description) {
         const menuItemDiv = document.createElement("div");
         const itemDiv = document.createElement("div");
         const priceDiv = document.createElement("div");
         const descriptionP = document.createElement("p");
 
-
-        const toppings = {
-            pizzaToppings: ["Pepperoni", "Ham", "Sausage", "Chicken", "Broccoli", "Green Pepper", "Red Pepper", "Banana Pepper", "Jalapeno Pepper", "Red Pepper","Green Olives", "Black Olives", "Onion", "Mushroom"],
-            pizzaSauce: ["Traditional", "Ranch", "Buffalo", "BBQ"],
-            wingFlavors: ["Hot Buffalo", "Mild Buffalo", "BBQ", "Honey BBQ", "Lemon Pepper", "Teriyaki"],
-            wingSauce: ["Ranch", "Blue Cheese"],
-            burgerToppings: ["Bacon", "Lettuce", "Tomato", "Onion", "Pickle"],
-            cheeseTypes: ["Provolone", "American", "Cheddar", "Pepper Jack"],
-            condiments: ["Ketchup", "Mustard", "Mayonnaise", "Relish", "Hot Sauce", "BBQ Sauce", "Melted Cheddar"],
-        };
-        
         this.item = item;
         this.price = price;
         this.description = description;
@@ -49,6 +49,25 @@ class menuItem {
 }
 
 export function menu() {
+    
+    const condDiv = document.createElement("div");
+    const condHead = document.createElement("h1")
+    const condList = document.createElement("ul");
+    
+    condHead.innerHTML = "Condiments";
+
+
+    toppings.condiments.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerHTML = item;
+        condList.appendChild(li);
+    })
+    
+    condDiv.appendChild(condHead);
+    condDiv.appendChild(condList);
+    content.appendChild(condDiv);
+    
+    
     new menuItem("pizza","2.00","")
     new menuItem("wings","10.00")
 }

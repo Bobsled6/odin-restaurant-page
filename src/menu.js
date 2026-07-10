cd981a12e5ff5d5d9b39c6d94eb5b7d605136908
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-10 19:29:14 -0400

    added menu categories and subMenu class to menu.js module

--- src/menu.js
+++ src/menu.js
@@ -31,7 +31,7 @@ class menuItem {
         itemDiv.innerHTML = item;
        
         priceDiv.classList.add("price");
-        priceDiv.innerHTML = '$' + price;
+        priceDiv.innerHTML = '$' + price + ".00";
         
         menuItemDiv.appendChild(itemDiv);
         menuItemDiv.appendChild(priceDiv);
@@ -58,21 +58,37 @@ class menuCategory {
         menuCatHead.innerHTML = name;
         menuCat.appendChild(menuCatHead);
 
-        const toppingList = document.createElement("ul");
-        toppings[toppingsCat].forEach((item) => {
-            let li = document.createElement("li");
-            li.innerHTML = item;
-            toppingList.appendChild(li);
-        })
-
-        console.log(menuCat);
+        if(toppingsCat != "null"){
+            const toppingList = document.createElement("ul");
+            toppings[toppingsCat].forEach((item) => {
+                let li = document.createElement("li");
+                li.innerHTML = item;
+                toppingList.appendChild(li);
+                menuCat.appendChild(toppingList);
+            })
+        
+        }
     
-       menuCat.appendChild(toppingList);
        menuCat.appendChild(descriptionP);
        content.appendChild(menuCat);
 
     }
 }
+
+class subCat {
+    constructor(name, parentCat) {
+        const parentCatDiv = document.getElementById(parentCat);
+        const subDiv = document.createElement("div");
+        const subHeader = document.createElement("h3");
+
+        subDiv.classList.add("subCat");
+        subDiv.id = name;
+        subHeader.innerHTML = name;
+        subDiv.appendChild(subHeader);
+        parentCatDiv.appendChild(subDiv);
+
+    }
+};
 
 export function menu() {
     
@@ -85,11 +101,26 @@ export function menu() {
     content.appendChild(menuHeader);
     
     new menuCategory("Pizza", "pizzaToppings","Available in slices (Cheese or Pepperoni), Small(12 in.), or Large(18 in.). Extra toppings $1.00 each");
-    new menuCategory("Condiments", "condiments", "")
     
-    new menuItem("Slice","2.00", "Pizza")
+    new menuCategory("Wings", "wingFlavors", "Available in bone-in or boneless, served with side of celery and choice of ranch or blue cheese dipping sauce");
+    new menuCategory("Burgers", "burgerToppings", "Served with choice of toppings and condiments on a brioche bun");
+    new menuCategory("French Fries", "null", "Salted Crispy Thin cut French Fries, available in regular or family sized portion");
+    new menuCategory("Hot Dog", "null", "Grilled Hot Dogs available in standard or foot long size, served with choice of condiments");
+    new menuCategory("Snacks and Treats", "null", "Tasty snacks when you just need a bite to make it through")
+    new menuCategory("Condiments", "condiments", "Free with purchase, Melted Cheddar limited to 1 serving per customer");
+
+    new subCat("Classic Bone-In", "Wings");
+
+    new menuItem("Slice","2", "Pizza")
     new menuItem("Small", "10", "Pizza")
     new menuItem("Large","15", "Pizza")
+
+    new menuItem("6 Ct","12", "Classic Bone-In")
+    new menuItem("12 Ct", "21", "Classic Bone-In" )
+    
+    
+
+    
 
     
 }

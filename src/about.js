cf03e4e997dd84efa20ac2c7a7e6210b12a54fdf
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-07-19 10:47:46 -0400

    slightly changed font color

--- src/about.js
+++ src/about.js
@@ -25,6 +25,7 @@ export function aboutDiv() {
     aboutButton.style.boxShadow = "inset 0px 0px 50px 0px #29CFFF, 0px 26px 9px -7px #185370";
 
     aboutHeader.innerHTML = "About Us";
+    aboutHeader.id = "aboutHeader";
     contactHeader.innerHTML = "Contact Us";
     aboutDesc.innerHTML = "Family owned since 1973. Ballbark Bar and Grill has continued to strive to serve the community and remain a positive place for the whole family."
     aboutDesc.id = "aboutDesc";
